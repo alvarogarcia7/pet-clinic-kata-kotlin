68b329e1637e40efbabd265129349421c7e1aafe
Alvaro Garcia <derecerca@gmail.com>
2018-11-24 14:55:33 +0100

    Refactor: move controller

--- src/main/kotlin/pet/clinic/infrastructure/delivery/VeterinariansController.kt
+++ src/main/kotlin/pet/clinic/infrastructure/delivery/VeterinariansController.kt
@@ -1,11 +1,10 @@
-package pet.clinic
+package pet.clinic.infrastructure.delivery
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Produces
-import pet.clinic.infrastructure.delivery.VeterinarianDTO
 
 @Controller("/veterinarians")
 class VeterinariansController {
